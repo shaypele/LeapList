5bb063bf2d5269d1740c7a2c084cbcfc136d212e
shaypele <shaypele@gmailc.om>
2014-12-31 20:49:03 +0200

    global fix

--- GlobalLock/src/leapListReg/LeapListDB.java
+++ GlobalLock/src/leapListReg/LeapListDB.java
@@ -8,14 +8,14 @@ import java.util.concurrent.locks.ReentrantLock;
 
 
 public class LeapListDB {
-	static final  int MAX_ROW;
+	static  int MAX_ROW;
 	public LeapList[] LeapLists;
 	
 	private final ReentrantLock dbLock = new ReentrantLock();
 	
 	public LeapListDB (int numberOfLists) {
 		MAX_ROW = numberOfLists;
-		leapLists = new LeapLists[MAX_ROW];
+		LeapLists = new LeapList[MAX_ROW];
 		for (int i=0; i < MAX_ROW ; i++)
 		{
 			LeapLists[i] = new LeapList();
